36b3132f0a19f4764d28a6e0e98fbd36171e850c
dbanyay <banyaydani@gmail.com>
2018-11-23 20:37:15 +0100

    final Lena

--- Project1/matlab/deblur.m
+++ Project1/matlab/deblur.m
@@ -1,23 +1,25 @@
 function im_deblur = deblur(im_bl,h,n_var)
 
 
-bl_var = var(double(im_bl(:)));
-
+bl_var = var(double(im_bl(:))); % pad and blur edges
 
 r_pad = size(h,1)-1;
 c_pad = size(h,2)-1;
 
-im_bl= padarray(im_bl,[r_pad c_pad],0,'both');
+im_bl= padarray(edgetaper(im_bl,h),[r_pad c_pad],0,'both');
 
 G=fftshift(fft2(im_bl));
 H=fftshift(fft2(h,size(im_bl,1),size(im_bl,2)));
 
 K = n_var/(bl_var-n_var);
+
+
 F=G.*H./(H.^2+(K));
 
 F=ifftshift(F);
 
-im_deblur=abs(ifft2(F));   
-imshow(uint8(im_deblur));
+im_deblur=abs(ifft2(F));
+
+im_deblur = im_deblur(r_pad+1:size(im_bl,1)-2*r_pad,c_pad+1:size(im_bl,2)-2*c_pad);
 
 end
--- Project1/matlab/main.m
+++ Project1/matlab/main.m
@@ -1,4 +1,4 @@
-close all
+%close all
 clear all
 
 %% 1. Open image
@@ -18,96 +18,92 @@ im_sp(n==1) = 255;
 
 %Reducing the contrast of Image
 
-% a = 0.2;
-% b = 50;
-% 
-% imRedDynRange = round(a*im + b);
-% imRedDynRange(imRedDynRange > 255) = 255;
-% imRedDynRange(imRedDynRange < 000) = 000;
-% 
-% %Histogram Equalization
-% [counts,binLocations] = imhist(im);
-% probaBin = 255*counts./(sum(counts));
-% equalizedValue = cumsum(probaBin);
-% eqIm = im;
-% 
-% for i = 1:numel(binLocations)
-%     eqIm(im == binLocations(i)) = equalizedValue(i);
-% end
+a = 0.2;
+b = 50;
 
-% %Plotting the Histograms
-% figure(1)
-% subplot(3,1,1)
-% imhist(im)
-% title('Original Image','fontweight','normal');
-% % bar('BarWidth', 0);
-% subplot(3,1,2)
-% imhist(imRedDynRange)
-% title('Reduced Contrast Image','fontweight','normal');
-% ylabel('h(r)')
-% subplot(3,1,3)
-% imhist(eqIm)
-% title('After Histogram Equalization','fontweight','normal');
-% xlabel('r')
+imRedDynRange = round(a*im + b);
+imRedDynRange(imRedDynRange > 255) = 255;
+imRedDynRange(imRedDynRange < 000) = 000;
+
+%Histogram Equalization
+[counts,binLocations] = imhist(im);
+probaBin = 255*counts./(sum(counts));
+equalizedValue = cumsum(probaBin);
+eqIm = im;
+
+for i = 1:numel(binLocations)
+    eqIm(im == binLocations(i)) = equalizedValue(i);
+end
+
+%Plotting the Histograms
+figure(1)
+subplot(3,1,1)
+imhist(im)
+title('Original Image','fontweight','normal');
+% bar('BarWidth', 0);
+subplot(3,1,2)
+imhist(imRedDynRange)
+title('Reduced Contrast Image','fontweight','normal');
+ylabel('h(r)')
+subplot(3,1,3)
+imhist(eqIm)
+title('After Histogram Equalization','fontweight','normal');
+xlabel('r')
+
+%Plotting the transfer function used for histogram equalization
+figure()
+stem(binLocations,equalizedValue)
+axis([0 255 0 255])
+xlabel('Gray Scale value in original Image')
+ylabel('Gray Scale value in transformed Image')
+title('Transfer function used for Equalization')
 % 
-% %Plotting the transfer function used for histogram equalization
-% figure()
-% stem(binLocations,equalizedValue)
-% axis([0 255 0 255])
-% xlabel('Gray Scale value in original Image')
-% ylabel('Gray Scale value in transformed Image')
-% title('Transfer function used for Equalization')
-% % 
-% % %Grayscale representation of Images
-% % figure(2)
-% % suptitle('Grayscale Representation')
-% % subplot(2,2,1)
-% % imshow(im)
-% % title('Original Image','fontweight','normal');
-% % subplot(2,2,2)
-% % imshow(imRedDynRange)
-% % title('After Contrast Reduction','fontweight','normal');
-% % subplot(2,2,3)
-% % imshow(eqIm);
-% % title('After Histogram Equalization','fontweight','normal');
-% 
-%Scale color representation of Images for Visual Inspection
-% figure(3)
+% %Grayscale representation of Images
+% figure(2)
+% suptitle('Grayscale Representation')
 % subplot(2,2,1)
-% imagesc(im, [0 255]);
-% colorbar;
+% imshow(im)
 % title('Original Image','fontweight','normal');
 % subplot(2,2,2)
-% imagesc(imRedDynRange, [0 255]);
-% colorbar;
+% imshow(imRedDynRange)
 % title('After Contrast Reduction','fontweight','normal');
 % subplot(2,2,3)
-% imagesc(eqIm, [0 255]);
-% colorbar;
+% imshow(eqIm);
 % title('After Histogram Equalization','fontweight','normal');
-% 
-% part02(im, im_sp);
-% 
+
+Scale color representation of Images for Visual Inspection
+figure(3)
+subplot(2,2,1)
+imagesc(im, [0 255]);
+colorbar;
+title('Original Image','fontweight','normal');
+subplot(2,2,2)
+imagesc(imRedDynRange, [0 255]);
+colorbar;
+title('After Contrast Reduction','fontweight','normal');
+subplot(2,2,3)
+imagesc(eqIm, [0 255]);
+colorbar;
+title('After Histogram Equalization','fontweight','normal');
+
+part02(im, im_sp);
+
 %% 3. Freqency domain filtering
-
-%im_bl = imread('boats512_outoffocus.bmp');
-
-
 
 h = myblurgen('gaussian',8);
 im_bl = conv2(im, h);
 
-noise_var = 0.0833;
+noise_var = 0.0833; % 
 
 im_deblur = deblur(im_bl,h,noise_var);
 
 figure
 subplot(2,2,2)
-imshow(im_deblur)
+imshow(uint8(im_deblur))
 title('Original image')
 
 subplot(2,2,1)
-imshow(im_bl)
+imshow(uint8(im_bl))
 title('Blurred image')
 
 subplot(2,2,3)
